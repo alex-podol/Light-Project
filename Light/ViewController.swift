968af49421ef449929745f1e908d043542763a26
Alexander <sany@alexanders-macbook-air.local>
2019-01-04 22:54:28 +0200

    Hide status bar and made torch

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -6,20 +6,35 @@
 //  Copyright © 2019 Alexander. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
-    var lightOn = false
+    var isLightOn = true
     
     @IBAction func buttonPressed(_ sender: UIButton) {
-        lightOn = !lightOn
+        isLightOn = !isLightOn
         
         updateUI()
     }
     
     func updateUI() {
-        view.backgroundColor = lightOn ? .white : .black
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if let dev = device, dev.hasTorch {
+            view.backgroundColor = .black
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = isLightOn ? .on : .off
+                dev.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        } else {
+            view.backgroundColor = isLightOn ? .white : .black
+        }
+        
     }
     
     override func viewDidLoad() {
@@ -27,6 +42,8 @@ class ViewController: UIViewController {
         updateUI()
     }
 
-
+    // swith off status-bar
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
 }
-
